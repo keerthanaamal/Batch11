6604d6ec168475781885da53d7bcd36b04a780a4
keerthana gopi <keerthanagopi112@gmail.com>
2024-05-23 17:46:47 +0530

    third commit

--- project/app/views.py
+++ project/app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def new(request):
-    print('.........................................')
+    # print('.........................................')
     return HttpResponse("hai")
