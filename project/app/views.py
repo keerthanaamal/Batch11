a16ac4af19e050afbad8c7c7c7e8c2a4278dc0cc
keerthana gopi <keerthanagopi112@gmail.com>
2024-05-24 11:55:41 +0530

    fourth commit

--- project/app/views.py
+++ project/app/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def new(request):
+    print("fourth changing happening")
     # print('.........................................')
     return HttpResponse("hai")
